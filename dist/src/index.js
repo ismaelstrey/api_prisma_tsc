8109151642f167e846244f52f5937dfba7650db0
ismael strey pereira <ismaelstrey@hotmail.com>
2023-06-06 08:17:20 -0300

    update .lck

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,10 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
-dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import router from "./routes";
 
-dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
